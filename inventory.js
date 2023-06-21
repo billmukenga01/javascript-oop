57fc0eeeab48f85e19341f805d664b9616832ddd
billmukenga01 <billmukenga01@gmail.com>
2023-06-21 17:14:57 +0300

    feat: complete program to save items to inventory

--- inventory.js
+++ inventory.js
@@ -1,6 +1,6 @@
 function Inventory(item, price) {
     // add code here
-    const inventory ={};
+    const inventory = {};
     
     inventory[item]={
       price: price,
@@ -16,21 +16,33 @@ function Inventory(item, price) {
         this.price = price;
       }
     };
-    inventory.prototype.deleteItem = function (){
-      
+    inventory.prototype.deleteItem = function (item){
+      if(this.hasOwnProperty(item)){
+        this.quantity -= 1;
+        return 'Deleted';
+      }else if(this.quantity === 0){
+        return 'Nothing to delete';
+      }
     };
-    inventory.prototype.checkItem = function (){
-      
+    inventory.prototype.checkItem = function (item){
+        if(this.hasOwnProperty(item)){
+            return this;
+        }else{
+            return 'Item is not in Inventory';
+        }      
     };
+
+
+    return inventory;
 }
   
 const myInventory = new Inventory('cucumber', 2);
   
 // Uncomment these lines to check your work!
-// myInventory.addItem('carrot', 1);
-// console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
-// myInventory.addItem('cucumber', 3); 
-// console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
-// console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
-// console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
-// console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
+myInventory.addItem('carrot', 1);
+console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
+myInventory.addItem('cucumber', 3); 
+console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
+console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
+console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
+console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
