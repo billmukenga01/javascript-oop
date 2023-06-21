28a6adda154299d80099f9ec85dbda3adaffff22
billmukenga01 <billmukenga01@gmail.com>
2023-06-21 17:34:28 +0300

    fix: __proto__ is depracated changed the code

--- inventory.js
+++ inventory.js
@@ -1,48 +1,25 @@
 function Inventory(item, price) {
     // add code here
-    const inventory = {};
+    const inventory = Object.create(Inventory.prototype);
     
-    inventory[item]={
-      price: price,
-      quantity: 1
-    };
-    
-    inventory.prototype.addItem = function (item, price){
-      if(this.hasOwnProperty(item)){
-        this.quantity +=1;
-        this.price = price;
-      }else{
-        this.item = item;
-        this.price = price;
-      }
-    };
-    inventory.prototype.deleteItem = function (item){
-      if(this.hasOwnProperty(item)){
-        this.quantity -= 1;
-        return 'Deleted';
-      }else if(this.quantity === 0){
-        return 'Nothing to delete';
-      }
-    };
-    inventory.prototype.checkItem = function (item){
-        if(this.hasOwnProperty(item)){
-            return this;
-        }else{
-            return 'Item is not in Inventory';
-        }      
-    };
-
+    inventory[item] = {
+        price: price,
+        quantity: 1
+    }
 
     return inventory;
+
 }
   
 const myInventory = new Inventory('cucumber', 2);
-  
+
+console.log(myInventory);
+
 // Uncomment these lines to check your work!
-myInventory.addItem('carrot', 1);
-console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
-myInventory.addItem('cucumber', 3); 
-console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
-console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
-console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
-console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
+// myInventory.addItem('carrot', 1);
+// console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
+// myInventory.addItem('cucumber', 3); 
+// console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
+// console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
+// console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
+// console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
